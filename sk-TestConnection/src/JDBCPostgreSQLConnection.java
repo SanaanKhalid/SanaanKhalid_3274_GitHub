566171a01b07cf33b8fa3217e3d98171be22f941
Sanaan Khalid <b2507417@ben.edu>
2022-11-29 10:32:09 -0600

    FinCommit

--- sk-TestConnection/src/JDBCPostgreSQLConnection.java
+++ sk-TestConnection/src/JDBCPostgreSQLConnection.java
@@ -1,14 +1,17 @@
 /***********************************************************
  * 
  * @author Sanaan Khalid
- * Date: 11/19/2022
+ * Date: 11/29/2022
  *
  **********************************************************/
 // package net.javaguides.postgresql.tutorial;
 
+import java.lang.Thread.State;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JDBCPostgreSQLConnection {
     // private final String url = "jdbc:postgresql://localhost/myDB";
@@ -45,6 +48,29 @@ public class JDBCPostgreSQLConnection {
      */
     public static void main(String[] args) {
         JDBCPostgreSQLConnection app = new JDBCPostgreSQLConnection();
-        app.connect();
+       Connection conn = app.connect();
+       int count = 0;
+       
+       try {
+    	   Statement stat = conn.createStatement();
+    	   stat.execute("drop table Test");
+    	   stat.execute("CREATE TABLE Test( Name VARCHAR(20))");
+    	   for(int i = 0; i < 10; i++) {
+    		   stat.execute("INSERT INTO Test VALUES('Romeo')");
+    	   }
+    	   stat.execute("INSERT INTO TEST VALUES ('Romeo') ");
+    	   ResultSet result = stat.executeQuery("SELECT * FROM Test");
+    	   while(result.next()) {
+    		   System.out.println(result.getString("Name"));
+    		   count++;
+    	   }
+    	   
+    	   System.out.println(count + "(rows)");
+    			   
+    	   
+       }
+       catch(SQLException e) {
+    	   System.out.println(e.getMessage());
+       }
     }
 }
